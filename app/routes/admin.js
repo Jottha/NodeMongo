d5262ec65f7f6e1dea999cca74f462d199b89a4e
Jailton Ventura <jottha_2006@hotmail.com>
2018-08-30 13:49:21 -0300

    Inserido registros no banco de dados MySqQL

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,13 +1,19 @@
-module.exports = function(app){
-    app.get('/formulario_inclusao_noticia', function (req, res) {
+module.exports = function(application){
+    application.get('/formulario_inclusao_noticia', function (req, res) {
         res.render('admin/form_add_noticia');
     });
     
 
-    app.post('/noticias/salvar', function (req, res) {
-        
-        var noticias = req.body;
+    application.post('/noticias/salvar', function (req, res) {
+      	var noticia = req.body;
 
-        res.send(noticias);
+		var connection = application.config.dbConnection();
+        var noticiasModel = application.app.models.noticiasModel;
+
+        noticiasModel.salvarNoticia(noticia, connection, function (error, result) {
+            //res.render('noticias/noticias', { noticias: result });
+            res.redirect('/noticias');
+        });
+
     });
 };

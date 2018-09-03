aeac8178602f5e43783f775450527ef295597c88
Jailton <jottha_2006@hotmail.com>
2018-09-03 17:11:28 -0300

    Implementando controllers

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,33 +1,10 @@
 module.exports = function(application){
     application.get('/formulario_inclusao_noticia', function (req, res) {
-        res.render('admin/form_add_noticia');
+        application.app.controllers.adminController.formulario_inclusao_noticia(application, req, res);
     });
     
 
     application.post('/noticias/salvar', function (req, res) {
-      	var noticia = req.body;
-
-        req.assert('titulo','Título é obrigatório').notEmpty();
-        req.assert('resumo','Resumo é obrigatório').notEmpty();
-        req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
-        req.assert('autor','Autor é obrigatório').notEmpty();
-        req.assert('data_noticia', 'Data é obrigatório').notEmpty();
-        req.assert('noticia','Noticia é obrigatória').notEmpty();
-
-        var erros = req.validationErrors();
-
-        if(erros){
-            res.render('admin/form_add_noticia');
-            return;
-        };
-
-		var connection = application.config.dbConnection();
-        var noticiasDAO = new application.app.models.NoticiasDAO(connection);
-
-        noticiasDAO.salvarNoticia(noticia, function (error, result) {
-            //res.render('noticias/noticias', { noticias: result });
-            res.redirect('/noticias');
-        });
-
+        application.app.controllers.adminController.noticias_salvar(application, req, res);
     });
 };
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,13 +1,10 @@
 module.exports = function (application) {
 
     application.get('/noticias', function (req, res) {
+        application.app.controllers.noticiasController.noticias(application, req, res);
+    });
 
-        var connection = application.config.dbConnection();
-        var noticiasDAO = new application.app.models.NoticiasDAO(connection);
-
-        noticiasDAO.getNoticias(function (error, result) {
-            res.render('noticias/noticias', { noticias: result });
-        });
-
+    application.get('/noticia', function (req, res) {
+        application.app.controllers.noticiasController.noticia(application, req, res);
     });
 };
